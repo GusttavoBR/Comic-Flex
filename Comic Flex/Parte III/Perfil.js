220ef2565b3e7f40ca9e0c3f3a8658461bd67186
Gustavo Vieira Arruda <90873790+GusttavoBR@users.noreply.github.com>
2022-11-29 17:09:34 -0300

    Rename 5 PERFIL.ALG to Perfil.js

---
+++ Comic Flex/Parte III/Perfil.js
@@ -0,0 +1,30 @@
+Algoritmo "5 Perfil"
+
+Var
+   inte, disc, med: inteiro
+   sobre: caracter
+
+
+Inicio
+   escreva("Digite o numero de interações que você teve: ")
+   leia(inte)
+
+   escreva("Digite o numero de discussões você criou: ")
+   leia(disc)
+
+   escreva("Digite algo sobre você: ")
+   leia(sobre)
+
+   escreva("Digite o número de medalhas que você possui: ")
+   leia(med)
+
+   limpatela
+   escreval(disc, " Discussão")
+   escreval(inte, " Interação")
+   escreval
+   escreval("Sobre você: ", sobre)
+   escreval
+   escreval(med, " Medalhas")
+
+
+Fimalgoritmo
