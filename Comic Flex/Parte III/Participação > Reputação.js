4f6002ecf729a6f3f71231644730cfb51fb25c0a
Gustavo Vieira Arruda <90873790+GusttavoBR@users.noreply.github.com>
2022-11-29 17:10:50 -0300

    Rename 9 PARTICIPAçãO_REPUTAçãO.ALG to Participação > Reputação.js

---
+++ Comic Flex/Parte III/Participação > Reputação.js
@@ -0,0 +1,8 @@
+Algoritmo "9 Participação_Reputação"
+
+Var
+
+Inicio
+   escreval("Você ainda não possui reputação pois seu numero de interações é baixo")
+   // sua reputação sobe na medida em que voce interage com outras pessoas
+fimalgoritmo
